a322e31188b18913f5a846df4410174b3233e659
Yari-Carelli <yaricarelli@hotmail.it>
2023-03-11 22:16:10 +0000

    Create Form, Posts and Post components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 import memories from './images/memories.png';
 
 const App = () => {
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Memories</Typography>
+        <img src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
+              <Posts />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
+              <Form />
             </Grid>
           </Grid>
         </Container>
