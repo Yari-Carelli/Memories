8ae917281fb11347afe7df3c8658bf869b393168
Yari-Carelli <yaricarelli@hotmail.it>
2023-03-11 21:02:05 +0000

    Create folder to store routes for back-end application

--- server/index.js
+++ server/index.js
@@ -3,15 +3,19 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.use('/posts', postRoutes);
+
 const CONNECTION_URL = 'mongodb+srv://yaricarelli:yaricarelli12345@cluster0.rghyb02.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 8080;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(error.message));
